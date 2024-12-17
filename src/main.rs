578dbcb6d0f19d656417d00c11d7967fde929d71
sevonj <100710152+sevonj@users.noreply.github.com>
2024-12-17 13:54:39 +0200

    add dir walk

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::env;
+use std::{env, fs::File, io::Write, path::PathBuf};
+
+use serde_json::json;
+use walkdir::WalkDir;
 
 fn main() {
     println!("### midi-msg file parser ###\n");
@@ -17,15 +20,73 @@ fn main() {
         return;
     }
 
-    let filepath = &args[1];
-
-    let Ok(bytes) = std::fs::read(filepath) else {
-        println!("Failed to read file.");
+    let path = PathBuf::from(&args[1]);
+    if !path.exists() {
+        println!("Path doesn't exist: {path:?}");
         return;
-    };
-    match midi_msg::MidiFile::from_midi(&bytes) {
-        Ok(_) => println!("Success!"),
-        Err(e) => println!("Failed:\n{}", e.to_string()),
     }
+
+    if path.is_file() {
+        let Ok(bytes) = std::fs::read(path) else {
+            println!("Failed to read file.");
+            return;
+        };
+        match midi_msg::MidiFile::from_midi(&bytes) {
+            Ok(_) => println!("Success!"),
+            Err(e) => println!("Failed:\n{}", e.to_string()),
+        }
+    } else {
+        let mut files_ok = vec![];
+        let mut files_err = vec![];
+        let mut files_noimp = vec![];
+        let mut files_skip = vec![];
+
+        for entry in WalkDir::new(path)
+            .into_iter()
+            .filter_map(std::result::Result::ok)
+        {
+            let path = entry.path();
+            print!("Attempting {path:?} ");
+            if path.is_file() && path.extension().is_some_and(|s| s == "mid") {
+                let Ok(bytes) = std::fs::read(path) else {
+                    println!("Failed to read file.");
+                    files_skip.push(path.to_string_lossy().to_string());
+                    continue;
+                };
+                match midi_msg::MidiFile::from_midi(&bytes) {
+                    Ok(_) => {
+                        println!("Success!");
+                        files_ok.push(path.to_string_lossy().to_string());
+                    }
+                    Err(e) => match e.error {
+                        midi_msg::ParseError::NotImplemented(_) => {
+                            println!("Not Implemented!");
+                            files_noimp.push(path.to_string_lossy().to_string())
+                        }
+                        _ => {
+                            println!("Error!");
+                            files_err.push(path.to_string_lossy().to_string());
+                        }
+                    },
+                }
+            }
+        }
+        println!("Files:");
+        println!("OK:       {}", files_ok.len());
+        println!("ERR:      {}", files_err.len());
+        println!("NoImp:    {}", files_noimp.len());
+        println!("Skipped:  {}", files_skip.len());
+
+        let data = json!({
+            "ok": files_ok,
+            "err": files_err,
+            "noimp": files_noimp,
+            "skip": files_skip,
+        });
+        let mut file = File::create("midi_msg_log.json").expect("File create fail");
+        file.write_all(data.to_string().as_bytes())
+            .expect("data write fail");
+    }
+
     println!("goodbye.");
 }
